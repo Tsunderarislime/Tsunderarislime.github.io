99adc059d624e55ac0f34dabdb3fc91222834374
Tsunderarislime <terrarislime@gmail.com>
2026-01-09 19:04:55 -0800

    Reformatted lots of text, removed redundant CSS

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Tsunderarislime.github.io
 My personal website which links a variety of projects I have worked on. My website is also an exercise in using HTML, CSS, and JavaScript. I implemented basic responsive web design, so that my website has a proper viewing experience no matter the platform.
 
-[Check it out](https://tsunderarislime.github.io)
+[Check it out!](https://tsunderarislime.github.io)
--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// Dictionary for controlling the big image with the small images in the control bar
-var dict = {
-    "1": ["assets/atsui-yo.webp", "https://github.com/Tsunderarislime/atsui-yo", "Atsui-yo", `This is my first Discord bot I developed, using Python and discord.py. 
-    It is a weather forecast bot which can provide a 12-hour forecast, 1-7 day forecast, as well as provide automated daily weather forecasts.
-    <br><br> Daily forecasts also come with a fun video depending on the highest temperature for that day. 
-    All of the functionality can be configured purely through using chat commands in a Discord channel.`],
-    "2": ["assets/yuuka.webp", "https://github.com/Tsunderarislime/yuuka", "Yuuka", `This is a Discord bot which is a proof of concept for a highly configurable, 
-    automated moderation tool. Its main goal is to automatically identify and ban scam advertisement messages. <br><br> 
-    A fully realized implementation of yuuka can be found in SFU Anime's own Discord bot, <a href='https://github.com/SFUAnime/Ren'>Ren.</a> 
-    Here, Yuuka was implemented as a cog called <a href='https://github.com/SFUAnime/Ren/tree/V3/testing/cogs/gatekeep'>\"Gatekeep\".</a> 
-    This cog implementation expands upon the functionality and customizablity of Yuuka. If your server uses the Red Discord Bot or any fork of it, 
-    then you can try my cog out by installing <a href='https://github.com/SFUAnime/lui-cogs-v3'>lui-cogs.</a>`],
-    "3": ["assets/perona-malik.webp", "https://github.com/Tsunderarislime/perona-malik-edge-enhancement", "Perona-Malik Edge Enhancement", `This is my implementation of 
-    the Perona-Malik equation to perform edge enhancement on images. Originally written in MATLAB, this script was written to showcase the algorithm with real examples 
-    for a numerical analysis project. I made this Python port because I want to eventually create a Flask web application where anyone can try my script online.`]
-};
-
 var m = document.getElementById("the-modal");
 var mc = document.getElementById("the-modal-content");
 
