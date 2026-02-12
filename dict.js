b71bfd90c8d04bd7dc54e12e5de21f4662178277
Tsunderarislime <terrarislime@gmail.com>
2026-02-11 21:29:44 -0800

    Added artist index, reordered projects by newest first

--- dict.js
+++ dict.js
@@ -1,17 +1,20 @@
 // Dictionary for controlling the main content image, title, and description
 var dict = {
     "1": ["assets/atsui-yo.webp", "https://github.com/Tsunderarislime/atsui-yo", "Atsui-yo", `This is my first Discord bot I developed, using Python and discord.py. 
-    It is a weather forecast bot which can provide a 12-hour forecast, 1-7 day forecast, as well as provide automated daily weather forecasts.
-    <br><br> Daily forecasts also come with a fun video depending on the highest temperature for that day. 
-    All of the functionality can be configured purely through using chat commands in a Discord channel.`],
+        It is a weather forecast bot which can provide a 12-hour forecast, 1-7 day forecast, as well as provide automated daily weather forecasts.
+        <br><br> Daily forecasts also come with a fun video depending on the highest temperature for that day. 
+        All of the functionality can be configured purely through using chat commands in a Discord channel.`],
     "2": ["assets/yuuka.webp", "https://github.com/Tsunderarislime/yuuka", "Yuuka", `This is a Discord bot which was a proof of concept for a highly configurable, 
-    automated moderation tool. Its main goal was to automatically identify and ban scam advertisement messages. <br><br> 
-    A fully realized implementation of Yuuka can be found in SFU Anime's own Discord bot, <a href='https://github.com/SFUAnime/Ren'>Ren.</a> 
-    Here, Yuuka was implemented as a cog called <a href='https://github.com/SFUAnime/Ren/tree/V3/testing/cogs/gatekeep'>\"Gatekeep\".</a> 
-    This cog implementation expanded upon the functionality and customizablity of Yuuka. If your server uses the Red Discord Bot or any fork of it, 
-    then you can try my cog out by installing <a href='https://github.com/SFUAnime/lui-cogs-v3'>lui-cogs.</a>`],
-    "3": ["assets/perona-malik.webp", "https://github.com/Tsunderarislime/perona-malik-edge-enhancement", "Perona-Malik Edge Enhancement", `This is my implementation of 
-    the Perona-Malik equation to perform edge enhancement on images. Originally written in MATLAB, this script was written to showcase the algorithm with real examples 
-    for a numerical analysis project. I have also built a simple <a href='https://github.com/Tsunderarislime/perona-malik-web'>Flask web application</a> 
-    to provide a nice graphical interface to run the algorithm.`]
+        automated moderation tool. Its main goal was to automatically identify and ban scam advertisement messages. <br><br> 
+        A fully realized implementation of Yuuka can be found in SFU Anime's own Discord bot, <a href='https://github.com/SFUAnime/Ren'>Ren.</a> 
+        Here, Yuuka was implemented as a cog called <a href='https://github.com/SFUAnime/Ren/tree/V3/testing/cogs/gatekeep'>\"Gatekeep\".</a> 
+        This cog implementation expanded upon the functionality and customizablity of Yuuka. If your server uses the Red Discord Bot or any fork of it, 
+        then you can try my cog out by installing <a href='https://github.com/SFUAnime/lui-cogs-v3'>lui-cogs.</a>`],
+    "3": ["assets/perona-malik.webp", "https://github.com/Tsunderarislime/perona-malik-edge-enhancement", "Perona-Malik Edge Enhancement", `This is my Python implementation of 
+        the Perona-Malik equation to perform edge enhancement on images. Originally written in MATLAB, this script was written to showcase the algorithm with real examples 
+        for a numerical analysis project.<br><br> I have also built a simple <a href='https://github.com/Tsunderarislime/perona-malik-web'>Flask web application</a> 
+    to provide a nice graphical interface to run the algorithm.`],
+    "4": ["assets/artist-index.webp", "https://artist-index.up.railway.app/", "Artist Index", `This is a Flask web application that hosts a database of artists 
+        I follow on various social media websites.<br><br>Operations like adding, removing, or editing entries can all be done through a custom user interface I built. 
+        For artists with Twitter accounts, there's additional functionality like fetching profile pictures and an embedded timeline.`]
 };
